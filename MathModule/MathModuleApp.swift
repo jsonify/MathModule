44108ba0067bcc841d91a5c80e0fa69bb278d827
Jason Rueckert <phlacin@gmail.com>
2020-12-02 13:08:23 -0800

    Initial Commit

---
+++ MathModule/MathModuleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathModuleApp.swift
+//  MathModule
+//
+//  Created by Jason Rueckert on 12/2/20.
+//
+
+import SwiftUI
+
+@main
+struct MathModuleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
